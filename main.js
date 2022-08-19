db62873b9afcf9633ddd48c0f83f21c03a756999
Gustavo Martin Alvarez <55332916+gustipardo@users.noreply.github.com>
2022-08-19 16:23:53 -0300

    Estetic nav changed + scrollbar delete + boton ubicar added

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 
+
+function ubicar(){
+    document.getElementById("mapa").src ="https://www.openstreetmap.org/export/embed.html?bbox=-58.70733261108399%2C-34.445445835453576%2C-58.69837403297425%2C-34.44166778346514&amp;layer=cyclosm";
+}
+
