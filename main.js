76e483e18ba7bd03e9d396ec50839e3966cbfffe
Gustavo Martin Alvarez <gusm2003@gmail.com>
2022-08-20 16:52:36 -0300

    Buttons Ubicar and ubicarPersonas added
    We added two buttons with their respective desing.
    Due to problems with OpenStretMap we chanche our layer to the estandar

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 
 
-function ubicar(){
-    document.getElementById("mapa").src ="https://www.openstreetmap.org/export/embed.html?bbox=-58.70733261108399%2C-34.445445835453576%2C-58.69837403297425%2C-34.44166778346514&amp;layer=cyclosm";
+function ubicarte(){
+    document.getElementById("mapa").src ="https://www.openstreetmap.org/export/embed.html?bbox=-58.70497226715088%2C-34.4461669196355%2C-58.69789123535157%2C-34.442105763118256&amp;layer=cyclosm";
+}
+function ubicarPersona(){
+    document.getElementById("mapa").src ="https://www.openstreetmap.org/export/embed.html?bbox=-58.58491256833077%2C-34.42146734881196%2C-58.58314231038094%2C-34.420451778091184&amp;layer=cyclosm";
 }
 
