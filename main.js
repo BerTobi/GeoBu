224afe65df3bb31533d0b0cf8596fbd7c1841cd3
Gustavo Martin Alvarez <55332916+gustipardo@users.noreply.github.com>
2022-08-22 11:02:32 -0300

    Map starting location fix
    I, Bertobi, hereby declare that the upcoming files have been uploaded by no other than myself, due to the exceptional situation of having to utilize this account instead of mine.

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+window.onload = function() {
+	if(!window.location.hash) {
+		window.location = window.location + '#loaded';
+		window.location.reload();
+	}
+}
+
 let Persona;
 let Hija;
 let Abuelos;
