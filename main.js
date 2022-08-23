9e3fdc0e8de38514f838403d739f012cfa15796f
Gustavo Martin Alvarez <55332916+gustipardo@users.noreply.github.com>
2022-08-23 16:22:20 -0300

    Some little changes on the js scrip

--- main.js
+++ main.js
@@ -3,24 +3,24 @@ window.onload = function() {
 		window.location = window.location + '#loaded';
 		window.location.reload();
 	}
-}
+} 
 
 let Persona;
 let Hija;
 let Abuelos;
 let Rotari;
 let YoMismo;
-Hija="https://www.openstreetmap.org/export/embed.html?bbox=-58.614383339881904%2C-34.45148859383025%2C-58.60542476177216%2C-34.44742769604025&amp;layer=mapnik"
-Abuelos="https://www.openstreetmap.org/export/embed.html?bbox=-58.63076090812684%2C-34.45990172103523%2C-58.6218023300171%2C-34.45584123234313&amp;layer=mapnik"
-Rotari="https://www.openstreetmap.org/export/embed.html?bbox=-58.58491256833077%2C-34.42146734881196%2C-58.58314231038094%2C-34.420451778091184&amp;layer=cyclosm"
-YoMismo="https://www.openstreetmap.org/export/embed.html?bbox=-58.70497226715088%2C-34.4461669196355%2C-58.69789123535157%2C-34.442105763118256&amp;layer=cyclosm";
+Hija="https://www.openstreetmap.org/export/embed.html?bbox=-58.614383339881904%2C-34.45148859383025%2C-58.60542476177216%2C-34.44742769604025&amp;layer=mapnik";    
+Abuelos="https://www.openstreetmap.org/export/embed.html?bbox=-58.63076090812684%2C-34.45990172103523%2C-58.6218023300171%2C-34.45584123234313&amp;layer=mapnik";
+Rotari="https://www.openstreetmap.org/export/embed.html?bbox=-58.585241138935096%2C-34.421810294744226%2C-58.5825589299202%2C-34.420186267372&amp;layer=mapnik&amp;marker=-34.4209982850011%2C-58.58390003442764";
+YoMismo="https://www.openstreetmap.org/export/embed.html?bbox=-58.7034434080124%2C-34.4456360607195%2C-58.69807898998261%2C-34.44264106846512&amp;layer=mapnik&amp;marker=-34.4441382594907%2C-58.70076274999997";
 Persona=YoMismo
 
 function ubicarPersonaSeleccionada(){
     document.getElementById("mapa").src =Persona;
 }
 function ubicarte(){
-    document.getElementById("mapa").src ="https://www.openstreetmap.org/export/embed.html?bbox=-58.70497226715088%2C-34.4461669196355%2C-58.69789123535157%2C-34.442105763118256&amp;layer=cyclosm";
+    document.getElementById("mapa").src =YoMismo;
 }
 
 function ubicarPersona(Familiar){
@@ -38,3 +38,4 @@ function jsFunction(value)
         Persona=Abuelos
     }
 }
+
