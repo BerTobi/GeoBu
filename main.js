a88c87aab9281f8b53fcdd6cf944e3a1caa612e0
Gustavo Martin Alvarez <55332916+gustipardo@users.noreply.github.com>
2022-08-26 16:24:53 -0300

    Big actualization on main.js

--- main.js
+++ main.js
@@ -1,15 +1,55 @@
-const tilesProvider= 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
- 
+let posicion
+var Localizaciones=[]
+Localizaciones[0]={nombre:"Rotary", lat: 34.4209568,lng: 58.5861718, zoom: 17.25}
+Localizaciones[1]={nombre:"Madre", lat: 34.4486057,lng: 58.6114588, zoom: 17.25}
+Localizaciones[2]={nombre:"Abuelo", lat: 34.4344941,lng: 58.6044944, zoom: 15.5}
+Localizaciones[3]={nombre:"Hija", lat: 34.5987012,lng: 58.50431, zoom: 17}
 
-let myMap = L.map('myMap').setView([51.505, -0.09], 13)
+let myMap = L.map('myMap').setView([-38.06, -58.26], 6)
 
-L.tileLayer(tilesProvider, {
-    maxZoom: 18,
-}).addTo(myMap)
+L.tileLayer(`https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`, {
+	maxZoom: 18,
+}).addTo(myMap);
 
+let marker = L.marker([51.5, -0.09]).addTo(myMap)
 
-let marker = L.marker([-34.45778082592164, -58.625149726867676].addTo(myMap))
-
-let iconMaker = L.icon({
-    // iconUrl = 'imagen/Maker.png'
+let iconMarker = L.icon({
+    iconUrl: 'imagen/Marker.png',
+    iconSize: [60, 60],
+    iconAnchor: [30, 60]
 })
+
+let marker2 = L.marker([51.51, -0.09], { icon: iconMarker }).addTo(myMap)
+
+myMap.doubleClickZoom.disable()
+myMap.on('dblclick', e => {
+  let latLng = myMap.mouseEventToLatLng(e.originalEvent);
+
+  L.marker([latLng.lat, latLng.lng], { icon: iconMarker }).addTo(myMap)
+})
+
+navigator.geolocation.getCurrentPosition(
+  (pos) => {
+    const { coords } = pos
+    const { latitude, longitude } = coords
+    posicion=coords
+    L.marker([latitude, longitude], { icon: iconMarker }).addTo(myMap)
+
+    setTimeout(() => {
+      myMap.flyTo(new L.LatLng(latitude, longitude))
+    }, 200)    
+  },
+  (error) => {
+    console.log(error)
+  },
+  {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  })
+function ubicarte(){
+    myMap.flyTo([posicion.latitude, posicion.longitude], 16)
+}
+function ubicarPersonaSeleccionada(valor){
+    myMap.flyTo([Localizaciones[valor].lat, Localizaciones[valor].lng], Localizaciones[valor].zoom)
+}
