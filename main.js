f9cf91b93221127217aa059d6132f4ccda19224c
Gustavo Martin Alvarez <55332916+gustipardo@users.noreply.github.com>
2022-08-29 14:22:53 -0300

    Granpa walking simulate + estetic changes
    Position form changed 
    function Personaenmovimiento finished
    NavBar Familiar type change

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-var watermark1 =document.getElementsByClassName('banner-content')[0];
-var watermark2 =document.getElementsByClassName('banner-content_hover')[0];
 let posicion
 var Localizaciones=[]
 Localizaciones[0]={nombre:"Rotary", lat: -34.4209733,lng: -58.5839837, zoom: 17.25}
 Localizaciones[1]={nombre:"Madre", lat: -34.4487165,lng: -58.6092354, zoom: 17.25}
-Localizaciones[2]={nombre:"Abuelo", lat: -34.4521504,lng: -58.6153645, zoom: 15.5}
+Localizaciones[2]={nombre:"Abuelo", lat: -34.4521504,lng: -58.6153645, zoom: 18}
 Localizaciones[3]={nombre:"Hija", lat: -34.4781203,lng: -58.6493504, zoom: 17}
 Localizaciones[4]={nombre:"Hijo", lat: -34.4781203,lng: -58.6493504, zoom: 17}
 var valornumerico
-
+var marcador
 let myMap = L.map('myMap').setView([-38.06, -58.26], 6)
+
+function change(iconID) {
+  var item = document.getElementById(iconID);
+  if (item) {}
+}
+window.onload = function(){
+  $('.message-box-buttons-panel__window-button').click()
+}
 
 L.tileLayer(`https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`, {
 	maxZoom: 18,
@@ -30,12 +36,6 @@ let iconFamiliarMarker = L.icon({
 
 let marker2 = L.marker([51.51, -0.09], { icon: iconMarker }).addTo(myMap)
 
-myMap.doubleClickZoom.disable()
-myMap.on('dblclick', e => {
-  let latLng = myMap.mouseEventToLatLng(e.originalEvent);
-
-  L.marker([latLng.lat, latLng.lng], { icon: iconMarker }).addTo(myMap)
-})
 
 navigator.geolocation.getCurrentPosition(
   (pos) => {
@@ -61,24 +61,40 @@ function ubicarte(){
 }
 function ubicarPersonaSeleccionada(valor){
   valornumerico=valor
+  if (valor==2) {
+    agregarmarcador()
+    myMap.flyTo([Localizaciones[2].lat, Localizaciones[2].lng], Localizaciones[2].zoom)
+    PersonaEnMovimiento()
+  }
+  else{
     myMap.flyTo([Localizaciones[valornumerico].lat, Localizaciones[valornumerico].lng], Localizaciones[valornumerico].zoom)
     L.marker([Localizaciones[valornumerico].lat, Localizaciones[valornumerico].lng], { icon: iconFamiliarMarker }).addTo(myMap)
-    if (valor=4) {PersonaEnMovimiento()}
+  }
 }
 function redirigir(){
   location.href = 'https://docs.google.com/document/d/1LBwLHVz0QG-0d4m2BbH3Q6RZwS_5odTy4TBWT5RcCi8/edit?usp=sharing';
 }
-function remover() {
-  var watermark1 =document.getElementsByClassName('banner-content')[0];
-var watermark2 =document.getElementsByClassName('banner-content_hover')[0];
-                watermark1.parentElement.remove(watermark1);
-                watermark2.parentElement.remove(watermark2);
-            }
+
 function PersonaEnMovimiento(){
   var movimiento = L.marker([Localizaciones[valornumerico].lat, Localizaciones[valornumerico].lng], { icon: iconFamiliarMarker }).addTo(myMap)
   myMap.removeLayer(movimiento)
 }
 
-function borrar(){
-  myMap.removeLayer(marker)
+function agregarmarcador(){
+   marcador = new L.marker([Localizaciones[2].lat, Localizaciones[2].lng], { icon: iconFamiliarMarker })
+   marcador.addTo(myMap)
 }
+function quitarmarcador(){
+  myMap.removeLayer(marcador)
+  Localizaciones[2].lat += 0.0005003
+  Localizaciones[2].lng += 0.0011203
+}
+function PersonaEnMovimiento(){
+  setInterval(() => {
+    quitarmarcador()
+    agregarmarcador()
+    myMap.flyTo([Localizaciones[2].lat, Localizaciones[2].lng], Localizaciones[2].zoom)
+  }, 7000)
+}
+
+
